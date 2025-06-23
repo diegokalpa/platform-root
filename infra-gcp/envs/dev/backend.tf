e1c7393596a16de9ae30970a1e5f0375beb9b1b1
dcor123 <dcoral@accela.com>
2025-06-23 11:55:33 -0700

    modify TF credentials

--- infra-gcp/envs/dev/backend.tf
+++ infra-gcp/envs/dev/backend.tf
@@ -1,19 +1,1 @@
-terraform {
-  required_version = ">= 1.7.0"
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "gcs" {
-    bucket = "dievops-tfstate"
-    prefix = "env/dev"
-  }
-}
